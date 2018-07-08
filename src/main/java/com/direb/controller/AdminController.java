16f659d9667e6d9ad2c5a0968587c4c6d45ccc2e
Iscool3301 <nakkazulav@mail.ru>
2018-07-08 22:39:31 +0400

    Was added mail service, and others small features

---
+++ src/main/java/com/direb/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.direb.controller;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/admin")
+@PreAuthorize("hasAuthority('ADMIN')")
+public class AdminController {
+
+    @GetMapping
+    public String mainAdmin() {
+        return "/admin";
+    }
+}
